3ac9f8fe55b3beaab7a3254d9c47cfea38a6184b
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2020-05-11 21:58:13 +0300

    Init stage 2

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,20 +1,63 @@
 package search;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Map<String, String> firstLine = new HashMap<>();
+        SearchEngine searchEngine = new SearchEngine();
+        searchEngine.enterData();
+        searchEngine.searchData();
+    }
+}
 
-        int index = 1;
-        for (String elem : scanner.nextLine().split("\\s")) {
-            firstLine.put(elem, Integer.toString(index));
-            index++;
+class SearchEngine {
+
+    Scanner scanner;
+    Set<String> dataSet;
+
+    public SearchEngine() {
+        this.scanner = new Scanner(System.in);
+        this.dataSet = new HashSet<>();
+    }
+
+    void enterData() {
+        System.out.println("Enter the number of people:");
+        int countLines = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter all people:");
+        for (int i = 0; i < countLines; i++) {
+            dataSet.add(scanner.nextLine());
         }
+    }
 
-        System.out.println(firstLine.getOrDefault(scanner.nextLine(), "Not Found"));
+    void searchData() {
+        System.out.println("\nEnter the number of search queries:");
+        int countSearch = Integer.parseInt(scanner.nextLine());
+        AtomicBoolean dataFound = new AtomicBoolean();
+
+        for (int i = 0; i < countSearch; i++) {
+            System.out.println("\nEnter data to search people:");
+            String searchData = scanner.nextLine();
+            StringBuilder foundData = new StringBuilder("\nFound people:");
+            dataFound.set(false);
+
+            dataSet.forEach(value -> {
+                int index = IntStream.range(0, value.toLowerCase().split("\\s").length)
+                        .filter(j -> searchData.toLowerCase().equals(value.toLowerCase().split("\\s")[j]))
+                        .findFirst()
+                        .orElse(-1);
+
+                if (index != -1) {
+                    foundData.append("\n").append(value);
+                    dataFound.set(true);
+                }
+            });
+            if (dataFound.get())
+                System.out.println(foundData);
+            else
+                System.out.println("No matching people found.");
+        }
     }
 }
